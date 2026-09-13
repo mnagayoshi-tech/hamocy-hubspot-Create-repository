1c3197aad8a0ec15cbf17125b626e57b4c4492cc
mnagayoshi-tech <m.nagayoshi@hamocy.com>
2026-09-13 20:12:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -173,20 +173,10 @@ def main():
 
         # 担当者をドロップダウンで選択
         OWNERS = {
-            "永芳昌裕 (Masahiro Nagayoshi)": "162107431",
-            "原田将寛 (Masahiro Harada)": "62352138",
-            "服部健太 (Kenta Hattori)": "82268477",
-            "大和田大輝 (Hiroki Owada)": "159639298",
-            "山西優真 (Yuma Yamanishi)": "160024980",
-            "林田恵里 (Eri Hayashida)": "160024983",
-            "平田俊一 (Shunichi Hirata)": "160024984",
-            "加藤正道 (Masamichi Kato)": "160024987",
-            "雑賀えみな (Emina Saiga)": "161694030",
-            "高橋光輝 (Koki Takahashi)": "162253556",
-            "北野ゆい (yui kitano)": "164430492",
-            "今井光太 (Kota Imai)": "164501619",
-            "細川莉子 (Riko Hosokawa)": "165897133",
-            "松尾泰洋 (Yasuhiro Matsuo)": "166746349",
+            "服部 健大": "82268477",
+            "平田 峻一": "160024984",
+            "細川 理子": "165897133",
+            "永芳 昌裕": "162107431",
         }
         selected_owner = st.selectbox("担当者を選択", list(OWNERS.keys()))
         oid = OWNERS[selected_owner]
